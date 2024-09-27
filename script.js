95ea0597fba7f3157600a0328605c3d909cf9721
Jacky Yang <jackyyang1670@gmail.com>
2024-09-26 20:37:44 -0700

    Updated files

--- script.js
+++ script.js
@@ -4,17 +4,19 @@
 // Use DOM manipulation to give a visual indication of the game's progress in some way.
 
 const categoryEl = document.querySelector('#category');
-const word = [...document.querySelectorAll('#word span')];
+const guessesEl = document.querySelector('#guesses');
+const letUsedEl = document.querySelector('#letters-used');
 const choices = {
   animals: ['lion', 'tiger', 'zebra', 'hippo', 'giraffe'],
   fruits: ['apple', 'banana', 'orange', 'pineapple', 'strawberry'],
   cities: ['new york', 'los angeles', 'chicago', 'houston', 'dallas'],
 }
-
+let guesses = 8;
 const getWord = () => {
   const category = prompt('Choose a category: 1 for animals, 2 for fruits, 3 for cities');
   let keys = Object.keys(choices);
   categoryEl.textContent = `Your chosen category is: ${keys[category-1][0].toUpperCase() + keys[category-1].slice(1)}`;
+  guessesEl.textContent = `You have ${guesses} guesses left.`;
   const word = choices[keys[category-1]][Math.floor(Math.random() * choices[keys[category-1]].length)];
   return word;
 }
@@ -32,37 +34,61 @@ const createWord = () => {
     div.appendChild(span);
     wordEl.appendChild(div);
   }
+  return word;
 }
 
 const getGuess = () => {
-  const guess = prompt('Guess a letter');
+  const guess = prompt('Guess a letter or the entire word (if you know it)');
   return guess.toLowerCase();
 }
 
 const checkGuess = () => {
   const guess = getGuess();
   
-  const guessEl = document.querySelector('#guesses');
   let guessBool = false;
-  word.forEach((letter) => {
+  
+  wordSpans.forEach((letter) => {
     if(guess === letter.textContent){
       letter.classList.remove('none');
       guessBool = true;
     }
+    else if(guess === word){
+      
+      wordSpans.forEach((letter) => {if(letter.classList.contains('none')) letter.classList.remove('none')});
+      guessBool = true;
+      setTimeout(() => alert('You won!🎉🎉'), 500);
+    }
   })
-  if(!guessBool) guessEl.textContent += guess;
+  if(!guessBool){
+    const span = document.createElement('span');
+    span.textContent = guess;
+    letUsedEl.appendChild(span);
+    checkLoss();
+  }
 }
 
 const areAllLettersGuessed = () => {
-  console.log(!(word.some((letter) => letter.classList.contains('none'))))
-  return !word.some((letter) => letter.classList.contains('none'));
+  return !wordSpans.some((letter) => letter.classList.contains('none'));
 }
 
 
 
 const checkWin = () => {
   checkGuess();
-  areAllLettersGuessed() ? alert('You won!') : checkWin();
+  setTimeout(() => {
+    areAllLettersGuessed() ? alert('You won!🎉🎉') : checkWin();
+  }, 1000);
+  
 }
-createWord();
-checkWin();
+
+const checkLoss = () => {
+  guesses--;
+  guessesEl.textContent = `You have ${guesses} guesses left.`;
+  if(guesses === 0) alert('You lost!😭😭');
+}
+const word =createWord();
+const wordSpans = [...document.querySelectorAll('#word span')];
+let div = document.createElement('div');  // Create the span element
+div.textContent = 'Letters used: ';        // Set the text content
+letUsedEl.appendChild(div);                // Append the span to the parent element
+setTimeout(checkWin, 1000);
